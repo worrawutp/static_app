c9d0cde8dd485bc93b22d9c5acd16200546e93e5
worrawut <pworrawut@hotmail.com>
2014-10-15 12:21:57 +0700

    finish static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+  describe "Home page" do
+    it "should have the content 'Sample App'" do
+      visit '/static_pages/home'    # use of Capybara function 'visit' to simpulate visiting URL
+      expect(page).to have_content('Sample App')  # use the 'page' variable in Capybara  
+    end
+  
+    it "should have the right title" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Tutorial: Ruby On Rails Sample App | Home")
+    end
+  end
+
+  describe "Help page" do
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/help'
+      expect(page).to have_title("Tutorial: Ruby On Rails Sample App | Help")
+    end
+  end
+
+  describe "About page" do
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/about'
+      expect(page).to have_title("Tutorial: Ruby On Rails Sample App | About")
+    end
+  end
+
+end
